77fb8149710ac05210f0e250b7ae785d7233f6cb
JorritMontijn <jorritmontijn@gmail.com>
2022-06-17 19:05:39 +0200

    Update PF_GetAreaPerCluster.m

--- subfunctions/PF_GetAreaPerCluster.m
+++ subfunctions/PF_GetAreaPerCluster.m
@@ -3,12 +3,19 @@ function [vecClustAreaId,cellClustAreaLabel,cellClustAreaFull] = PF_GetAreaPerCl
 	
 	%calculate cluster locations along probe trajectory
 	probe_area_ids = sProbeCoords.sProbeAdjusted.probe_area_ids_per_depth;
+	probe_area_labels = sProbeCoords.sProbeAdjusted.probe_area_labels_per_depth;
+	probe_area_full = sProbeCoords.sProbeAdjusted.probe_area_full_per_depth;
+	
 	intCurrentProbeLength = floor(sProbeCoords.sProbeAdjusted.probe_vector_sph(end));
+	probe_area_ids((end+1):intCurrentProbeLength) = probe_area_ids(end);
+	probe_area_labels((end+1):intCurrentProbeLength) = probe_area_labels(end);
+	probe_area_full((end+1):intCurrentProbeLength) = probe_area_full(end);
+	
 	vecVoxelDepth = (vecDepth / sProbeCoords.ProbeLengthMicrons)*intCurrentProbeLength;
 	vecVoxelIdx = min(max(round(vecVoxelDepth),1),intCurrentProbeLength);
 	
 	%get outputs per cluster
 	vecClustAreaId = probe_area_ids(vecVoxelIdx);
-	cellClustAreaLabel = sProbeCoords.sProbeAdjusted.probe_area_labels_per_depth(vecVoxelIdx);
-	cellClustAreaFull = sProbeCoords.sProbeAdjusted.probe_area_full_per_depth(vecVoxelIdx);
+	cellClustAreaLabel = probe_area_labels(vecVoxelIdx);
+	cellClustAreaFull =probe_area_full(vecVoxelIdx);
 end
