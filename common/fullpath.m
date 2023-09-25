e3ff9ddd1037ce6fd5f4bc774635a0a07a4fc715
Jorrit Montijn <jorritmontijn@gmail.com>
2023-09-25 13:38:48 +0200

    Update fullpath.m

--- common/fullpath.m
+++ common/fullpath.m
@@ -2,7 +2,13 @@ function strFullpath = fullpath(varargin)
 	%fullpath Like fullfile but also works for relative network paths
 	%   strFullpath = fullpath(varargin)
 	
+	if numel(varargin{1})>1 && strcmp(varargin{1}(1:2),[filesep filesep])
+		boolLeadingDouble = true;
+	end
 	strLead = strrep(varargin{1},[filesep filesep],filesep);
+	if boolLeadingDouble
+		strLead = strcat(filesep,strLead);
+	end
 	if ~isempty(strLead) && strLead(1) ~= 0 && ~strcmp(strLead(end),filesep),strLead(end+1)=filesep;end
 	sWarn = warning('off','MATLAB:deblank:NonStringInput');
 	strFullpath = strcat(strLead,fullfile(varargin{2:end}));
