572905906e2b1f4dbf37c138b497f67bf7b74c2e
JorritMontijn <jorritmontijn@gmail.com>
2024-02-08 10:50:02 +0100

    missing dependency

---
+++ common/cell2vec.m
@@ -0,0 +1,24 @@
+function vecOut = cell2vec(cellIn)
+	%cell2vec Transforms cell array to vector. Syntax:
+	%   vecOut = cell2vec(cellIn)
+	
+	if isempty(cellIn)
+		vecOut = [];
+		return;
+	end
+	
+    try
+        intElements = sum(cellfun(@numel,cellIn),'all');
+    catch
+        intElements = sum(cellfun(@numel,cellIn(:)));
+    end
+	vecOut = nan(intElements,1);
+	intElCounter = 1;
+	for intCell=1:numel(cellIn)
+		vecData = cellIn{intCell}(:);
+		intCellSize = numel(vecData);
+		vecOut(intElCounter:(intElCounter+intCellSize-1)) = vecData;
+		intElCounter = intElCounter + intCellSize;
+	end
+end
+
