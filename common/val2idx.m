784fdbd824175895e15956ab5939bcf725bfed61
JorritMontijn <jorritmontijn@gmail.com>
2024-02-29 17:29:00 +0100

    Update val2idx.m

--- common/val2idx.m
+++ common/val2idx.m
@@ -4,6 +4,15 @@ function [varDataOut,vecUnique,vecCounts,cellSelect,vecRepetition] = val2idx(var
 	%
 	%Used to be label2idx, but matlab now has a built-in with that name..
 	
+	%try converting to vector
+	try
+		varData2 = cell2vec(varData);
+		if numel(varData2) == numel(varData)
+			varData = varData2;
+		end
+	catch
+		%otherwise, don't do any transformations
+	end
 	varDataOut = nan(size(varData));
 	vecUnique = unique(varData);
 	vecCounts = zeros(size(vecUnique));
